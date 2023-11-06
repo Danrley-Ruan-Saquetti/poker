4377816b8a2432a6afae79229fdf98353dd7fb98
Dan Ruan <101991292+Danrley-Ruan-Saquetti@users.noreply.github.com>
2023-11-06 20:12:36 +0000

    add module deck

--- src/app/modules/card/card.model.ts
+++ src/app/modules/card/card.model.ts
@@ -1,3 +1,4 @@
+import { ID } from '@@types/index'
 import { Document } from '@esliph/repository-memory'
 
 export enum CardSuit {
@@ -8,8 +9,10 @@ export enum CardSuit {
 }
 
 export interface CardModel {
+    deckId: ID
     number: number
     suit: CardSuit
+    inDeck: boolean
 }
 
 export type Card = Document<CardModel>
--- src/app/modules/room/room.model.ts
+++ src/app/modules/room/room.model.ts
@@ -1,11 +1,5 @@
+import { ID } from '@@types/index'
 import { Document } from '@esliph/repository-memory'
-
-/*
-A criação de uma sala deve conter um identificador único, o valor da aposta mínimo, o valor da aposta atual da rodada,
-o valor do pote, o atual Dealer (sua ordem na mesa), o atual Small Blind (sua ordem na mesa),
-o atual Big Blind (sua ordem na mesa), o atual jogador a fazer o lance (sua ordem na mesa), o tempo limite para cada jogador
- realizar o lance, o baralho de cartas e a lista com a sequência da ordem dos jogadores do giro
-*/
 
 export type RoomModel = {
     minimumBetAmount: number
@@ -16,7 +10,7 @@ export type RoomModel = {
     orderCurrentBigBlind: number
     orderCurrentBidding: number
     betTimeout: number
-    deckCards: number[]
+    deckId: ID
 }
 
 export type Room = Document<RoomModel>
